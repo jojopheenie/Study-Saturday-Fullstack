e8754adfaf77ea033c522956af04241eceb0b48a
Josephine Amos <josephinexamos@gmail.com>
2020-10-17 13:49:53 -0400

    cycle 2

--- browser/components/Main.js
+++ browser/components/Main.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -6,68 +8,69 @@ import SingleStudent from './SingleStudent.js';
 import NewStudentForm from './NewStudentForm.js';
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [],
-      selectedStudent: {},
-      showStudent: false,
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			students: [],
+			selectedStudent: {},
+			showStudent: false,
+		};
 
-    this.selectStudent = this.selectStudent.bind(this);
+		this.selectStudent = this.selectStudent.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    // this.addStudent = this.addStudent.bind(this);
+	}
+
+	componentDidMount() {
+		this.getStudents();
+	}
+
+	async getStudents() {
+		console.log('getStudents fetching');
+		try {
+			const { data } = await axios.get('/student');
+			this.setState({ students: data });
+			console.log('THis is the State', this.state);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	selectStudent(student) {
+		return this.setState({
+			selectedStudent: student,
+		});
   }
 
-  componentDidMount() {
-    this.getStudents();
-  }
+	handleClick(e) {
+		return this.setState({
+			showStudent: !this.state.showStudent,
+		});
+	}
 
-  async getStudents() {
-    console.log('fetching');
-    try {
-      const { data } = await axios.get('/student');
-      this.setState({ students: data });
-      console.log('THis is the State', this.state);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  selectStudent(student) {
-    return this.setState({
-      selectedStudent: student,
-    });
-  }
-
-  handleClick(e) {
-    return this.setState({
-      showStudent: !this.state.showStudent,
-    });
-  }
-
-  render() {
-    console.log('this is the state in main', this.state);
-    return (
-      <div>
-        <h1>Students</h1>
-        <button onClick={this.handleClick}>Add Student</button>
-        {this.state.showStudent ? <NewStudentForm /> : null}
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Tests</th>
-            </tr>
-          </thead>
-          <StudentList
-            students={this.state.students}
-            selectStudent={this.selectStudent}
-          />
-        </table>
-        {this.state.selectedStudent.id ? (
-          <SingleStudent student={this.state.selectedStudent} />
-        ) : null}
-      </div>
-    );
-  }
+	render() {
+		console.log('this is the state in main', this.state);
+		return (
+			<div>
+				<h1>Students</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Tests</th>
+						</tr>
+					</thead>
+					<StudentList
+						students={this.state.students}
+						selectStudent={this.selectStudent}
+					/>
+				</table>
+				{this.state.selectedStudent.id ? (
+					<SingleStudent student={this.state.selectedStudent} />
+				) : null}
+				<button onClick={this.handleClick}>Add Student</button>
+				{this.state.showStudent ? <NewStudentForm /> : null}
+			</div>
+		);
+	}
 }
--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 
 export default class NewStudentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state)
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: ''
+    })
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName} />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName} />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input type="email" name="email" onChange={this.handleChange} value={this.state.email} />
         </label>
 
-        <button type="submit">Submit New Student</button>
+        <button type="submit" onClick={this.handleSubmit}>Submit New Student</button>
       </form>
     );
   }
